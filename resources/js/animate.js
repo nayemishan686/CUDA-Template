fcc95f0f9627eb7fea14667cc2b6211a3bd4b4f2
nayemishan686 <63306734+nayemishan686@users.noreply.github.com>
2020-04-07 22:04:11 +0600

    added html and css file for this website

---
+++ resources/js/animate.js
@@ -0,0 +1,12 @@
+ $(document).ready(function() {
+     // sticky menu
+
+     // protfilo-section
+     var mixer = mixitup('.container');
+ });
+ function openNav() {
+     document.getElementById("my-nav").style.width="100%";
+ }
+ function closeNav() {
+     document.getElementById("my-nav").style.width="0%";
+ }
